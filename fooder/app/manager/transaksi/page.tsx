072d6754272e923c95fcf091730a23135ffa2207
ryohariyono17 <ryohariyono17@gmail.com>
2025-01-22 09:58:57 +0000

    haii17

--- fooder/app/manager/transaksi/page.tsx
+++ fooder/app/manager/transaksi/page.tsx
@@ -1,4 +1,9 @@
+"use client"
+
+import { useState } from "react";
+
 const TransaksiPage = () => {
+  const [progress, setProgress] = useState(50);
   return (
     <div className="justify-center bg-white mx-6">
       <div className="px-5  w-auto rounded-xl mb-12 flex">
@@ -33,15 +38,20 @@ const TransaksiPage = () => {
 
             <div className="flex gap-2">
               <div className="rounded-full w-6 h-6 bg-black" />
-              <div>
-                <p className="font-semibold text-lg mb-1">Rp 12,000,000</p>
+              <div className="h-14 bg-red-300">
+                <p className="font-semibold text-lg mb-auto">Rp 12,000,000</p>
                 <p className="text-xs text-gray-400">Jumlah Pendapatan Setiap Bulan</p>
               </div>
             </div>
 
-            <div className="flex gap-2">\
+            <div className="flex gap-2">
               <div>
-                
+                <div className="w-full bg-gray-200 rounde h-6 mb-2">
+                  <div
+                    className="bg-[#F45846] h-4 rounded transition-all duration-300"
+                    style={{ width: `${progress}%` }}
+                  ></div>
+                </div>
                 <p className="text-xs text-gray-400">Jumlah Pendapatan Setiap Bulan</p>
               </div>
             </div>
